9d8b10f983d7796be6ceb75aea5da90e5999f6c9
natalybeltran1414-source <natalybeltran1414@gmail.com>
2025-09-29 17:46:59 -0500

    cambios generales

---
+++ aprendizaje/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AprendizajeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aprendizaje'
--- diagnostico/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, "diagnostico/index.html")
-
-def detalle(request, pk):
-    return render(request, "diagnostico/detalle.html", {"pk": pk})
